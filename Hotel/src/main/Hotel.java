3fc4b0f6707b71fc033db23ed56d0babc19ec1c7
Michal Szura <michal.szura@gmail.com>
2014-10-06 18:45:35 +0200

    Setup Commit IntelliJ

---
+++ Hotel/src/main/Hotel.java
@@ -0,0 +1,23 @@
+package main;
+
+/**
+ * Created by K O M P U T E R on 2014-10-06.
+ */
+public class Hotel {
+
+    /**
+     *
+     */
+    public Hotel() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args Starting parameter
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
